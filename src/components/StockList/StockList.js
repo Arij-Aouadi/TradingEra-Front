50b1ddf35a55638effc860fc950ba8e475752710
mohamedmoetez.belhout@esprit.tn <mohamedmoetez.belhout@esprit.tn>
2023-11-01 23:23:54 +0100

    listedesactions

--- src/components/StockList/StockList.js
+++ src/components/StockList/StockList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axiosInstance from '../../axios';
+import { FaBuilding, FaApple, FaGoogle } from 'react-icons/fa';
 
 function StockList() {
   const [stocks, setStocks] = useState([]);
@@ -15,13 +16,25 @@ function StockList() {
       });
   }, []);
 
+  const getIconForCompany = (companyName) => {
+    if (companyName === 'google') {
+      return <FaGoogle />;
+    } else if (companyName === 'apple') {
+      return <FaApple />;
+    } else if (companyName === 'COMP1') {
+      return <FaBuilding />;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div>
-      <h1>Liste de toutes les actions</h1>
+      <h5>Liste de toutes les actions</h5>
       <ul>
         {stocks.map((stock) => (
           <li key={stock.idA}>
-            {stock.symbole} - Cours : {stock.coursActuel} - Variation : {stock.variationEnPorcentage}
+            {getIconForCompany(stock.name)} {stock.symbole} - Cours : {stock.coursActuel} - Variation : {stock.variationEnPorcentage}
           </li>
         ))}
       </ul>
