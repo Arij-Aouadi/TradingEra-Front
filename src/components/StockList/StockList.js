803337c55de55584c3a93497280407c39a44fd68
mohamedmoetez.belhout@esprit.tn <mohamedmoetez.belhout@esprit.tn>
2023-11-26 18:27:02 +0100

    changement de pc

--- src/components/StockList/StockList.js
+++ src/components/StockList/StockList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axiosInstance from '../../axios';
-import { FaBuilding, FaApple, FaGoogle } from 'react-icons/fa';
+import { FaBuilding, FaApple, FaGoogle , FaMicrosoft, FaAmazon, FaFacebook,FaTwitter,FaAndroid,FaInstagram,FaAirbnb,FaSnapchat, FaUber, FaPaypal,FaEtsy ,FaSquare} from 'react-icons/fa';
 
 function StockList() {
   const [stocks, setStocks] = useState([]);
@@ -23,21 +23,63 @@ function StockList() {
       return <FaApple />;
     } else if (companyName === 'COMP1') {
       return <FaBuilding />;
-    } else {
+    } else if (companyName === 'microsoft') {
+      return <FaMicrosoft />;
+    } else if (companyName === 'amazon') {
+      return <FaAmazon />;
+    } else if (companyName === 'facebook') {
+      return <FaFacebook />;
+    } else if (companyName === 'android') {
+      return <FaAndroid />;
+    } else if (companyName === 'twitter') {
+      return <FaTwitter />;
+    }
+    
+    else if (companyName === 'instagram') {
+      return <FaInstagram />;
+    } else if (companyName === 'Airbnb') {
+      return <FaAirbnb />;
+    } 
+    else if (companyName === 'snapchat') {
+      return <FaSnapchat />;
+    }  else if (companyName === 'uber') {
+      return <FaUber />;
+    } else if (companyName === 'paypal') {
+      return <FaPaypal />;
+    }
+    else if (companyName === 'eatsy') {
+      return <FaEtsy />;
+    }
+    else if (companyName === 'square') {
+      return <FaSquare />;
+    } 
+    else {
       return null;
     }
   };
 
   return (
-    <div>
-      <h5>Liste de toutes les actions</h5>
-      <ul>
-        {stocks.map((stock) => (
-          <li key={stock.idA}>
-            {getIconForCompany(stock.name)} {stock.symbole} - Cours : {stock.coursActuel} - Variation : {stock.variationEnPorcentage}
-          </li>
-        ))}
-      </ul>
+    <div style={{ maxHeight: '400px', overflowY: 'auto' }}>
+      <table style={{ borderSpacing: '0 10px' }}>
+        <thead>
+          <tr>
+            <th style={{ padding: '10px' }}></th>
+            <th style={{ padding: '10px' }}>symbole</th>
+            <th style={{ padding: '10px' }}>Cours Actuel</th>
+            <th style={{ padding: '10px' }}>Variation en Pourcentage</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocks.map((stock) => (
+            <tr key={stock.idA}>
+              <td style={{ padding: '10px' }}>{getIconForCompany(stock.name)}</td>
+              <td style={{ padding: '10px' }}>{stock.symbole}</td>
+              <td style={{ padding: '10px' }}>{stock.coursActuel}</td>
+              <td style={{ padding: '10px' }}>{stock.variationEnPorcentage}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
